84a053e2a7851a58c4ce59ef04bbc48f33a5c3ce
LaetiTcha <laetitia.pham-van-chien@epitech.eu>
2023-03-07 19:27:44 +0100

    🔥 F01: Delete App.test.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  // state (état, données)
+  const [name, setName] = useState("Laetitia");
+  const [count, setCount] = useState(1);
+
+
+  // comportements
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+
+  // affichage (render)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bonjour {name}</h1>
+      <p>{count}</p>
+      <button onClick={handleClick}>Incrementer</button>
     </div>
+    
   );
 }
 
