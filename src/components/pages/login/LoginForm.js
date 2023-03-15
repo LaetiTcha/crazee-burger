d7594562d56ef20dec40e5b51fd9a3f81b1d9d38
LaetiTcha <laetitia.pham-van-chien@epitech.eu>
2023-03-15 17:34:34 +0100

    💄 F03: LoginPage > add layout

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function LoginForm() {
     // state
@@ -19,12 +20,18 @@ export default function LoginForm() {
 
     // affichage    
     return (
-        <form action="submit" onSubmit={handleSubmit}>
+        <LoginFormStyled action="submit" onSubmit={handleSubmit}>
             <h1>Bienvenue chez nous !</h1>
             <br/>
             <h2>Connectez-vous</h2>
             <input value={inputValue} onChange={handleChange} type={"text"} placeholder={"Entrez votre prénom..."} required/>
             <button>Accédez à votre espace</button>
-        </form>
+        </LoginFormStyled>
   )
 }
+
+const LoginFormStyled = styled.form`
+  background: green;
+  display: flex;
+  flex-direction: column;
+`;
