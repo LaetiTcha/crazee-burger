84e70000a60a05209a606034746800ec9eb753ad
LaetiTcha <laetitia.pham-van-chien@epitech.eu>
2023-03-16 17:15:36 +0100

    💄 F03: LOGIN > LoginForm > input > Add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import {BsPersonCircle} from 'react-icons/bs';
 
 export default function LoginForm() {
     // state
@@ -24,7 +25,10 @@ export default function LoginForm() {
             <h1>Bienvenue chez nous !</h1>
             <hr/>
             <h2>Connectez-vous</h2>
-            <input value={inputValue} onChange={handleChange} type={"text"} placeholder={"Entrez votre prénom..."} required/>
+            <div className="input-with-icon">
+              <BsPersonCircle className="icon"/>
+              <input value={inputValue} onChange={handleChange} type={"text"} placeholder={"Entrez votre prénom..."} required/>
+            </div>
             <button>Accédez à votre espace</button>
         </LoginFormStyled>
   )
@@ -55,5 +59,31 @@ const LoginFormStyled = styled.form`
     margin: 20px 10px 10px;
     color: white;
     font-size: 36px;
+  }
+
+  .input-with-icon {
+    background-color: #fff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding:  18px 24px;
+    margin: 18px 0;
+
+    .icon {
+      font-size: 15px;
+      margin-right: 8px;
+      color: #93a2b1;
+    }
+
+    input {
+      border: none;
+      font-size: 15px;
+      color: #17161a;
+    }
+
+    &::placeholder {
+      background: white;
+      color: lightgrey;
+    }
   }
 `;
