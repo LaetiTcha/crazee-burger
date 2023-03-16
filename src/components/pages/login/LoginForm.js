394ff57a66f27ecc3f0cfa80471d552950b79534
LaetiTcha <laetitia.pham-van-chien@epitech.eu>
2023-03-16 16:48:04 +0100

    💄 F03: LOGIN > LoginForm > Add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -22,7 +22,7 @@ export default function LoginForm() {
     return (
         <LoginFormStyled action="submit" onSubmit={handleSubmit}>
             <h1>Bienvenue chez nous !</h1>
-            <br/>
+            <hr/>
             <h2>Connectez-vous</h2>
             <input value={inputValue} onChange={handleChange} type={"text"} placeholder={"Entrez votre prénom..."} required/>
             <button>Accédez à votre espace</button>
@@ -32,6 +32,28 @@ export default function LoginForm() {
 
 const LoginFormStyled = styled.form`
   background: green;
-  display: flex;
-  flex-direction: column;
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0 auto;
+  padding: 2.5rem 2rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
+  
+  hr {
+    border: 1.5px solid #f56a2c;
+    margin-bottom: 40px;
+  }
+
+  h1 {
+    color: white;
+    font-size: 48px;
+  }
+
+  h2 {
+    color: #8e8b8b;
+    margin: 20px 10px 10px;
+    color: white;
+    font-size: 36px;
+  }
 `;
