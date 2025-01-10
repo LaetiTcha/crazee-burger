18965bbaff91d385a598f93a801a53fa6f5a9357
laetitcha <37798267+LaetiTcha@users.noreply.github.com>
2025-01-10 16:14:08 +0000

    💄 F05: ORDER > MENU > Add final style

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import { theme } from "../../../../theme/index";
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
@@ -15,7 +16,8 @@ export default function Menu() {
 }
 
 const MenuStyled = styled.div`
-  background: purple;
+  background: ${theme.colors.background_white};
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 60px;
